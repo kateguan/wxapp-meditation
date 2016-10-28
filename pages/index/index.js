5838fd56503a8f4405647bbac626ffeeddd9be61
formaction <formaction@163.com>
2016-10-28 20:11:43 +0800

    冥想

---
+++ pages/index/index.js
@@ -0,0 +1,76 @@
+//index.js
+//获取应用实例
+var app = getApp();
+
+var imgIndex = 0,
+    playing = false,
+    audioUrls = [
+        'https://dn-working-noise.qbox.me/noise/min/JapaneseGarden.mp3',
+        'https://dn-working-noise.qbox.me/noise/min/Forest.mp3',
+        'https://dn-working-noise.qbox.me/noise/min/Ocean.mp3',
+        'https://dn-working-noise.qbox.me/noise/min/Rain.mp3',
+        'https://dn-working-noise.qbox.me/noise/min/CoffeeShop.mp3',
+    ];
+
+Page({
+    data: {
+        imgUrls: [
+        'https://dn-working-noise.qbox.me/images/garden.jpg',
+        'https://dn-working-noise.qbox.me/images/ocean.png',
+        'https://dn-working-noise.qbox.me/images/forest.jpg',
+        'https://dn-working-noise.qbox.me/images/rain.jpg',
+        'https://dn-working-noise.qbox.me/images/cafe.jpg'
+    ],
+    duration: 1000,
+    src: '',
+    userInfo: {}
+    },
+
+    onLoad: function () {
+        this.setData({
+            userInfo: app.globalData.userInfo
+        })
+
+    },
+
+    onReady: function (e) {
+        // 使用 wx.createAudioContext 获取 audio 上下文 context
+        this.audioCtx = wx.createAudioContext('myAudio')
+    },
+
+    intervalChange: function(e) {
+        console.log('111');
+        imgIndex = e.detail.current;
+        
+        this.audioCtx.pause();
+        playing = false;
+    },
+
+    tapImgItem: function () {
+        if ( playing ) {
+            this.audioCtx.pause();
+            playing = false;
+        }
+    },
+
+    changeIndicatorDots: function(e) {
+        if ( playing ) {
+            return;
+        }
+        console.log(audioUrls[imgIndex])
+
+        this.setData({
+            src: audioUrls[imgIndex],
+        });
+
+        this.audioCtx.play();
+        playing = true;
+    },
+
+    bindViewTap: function () {
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
+    },
+
+})
