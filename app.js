fc4f333771486c4d2f5bdf3ae9ae997348e6bd35
formaction <formaction@163.com>
2016-11-08 11:12:49 +0800

    解决用户信息不出现及若干更新

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
 //app.js
 App({
+
+  globalData:{
+    userInfo: null
+  },
+
   onLaunch: function () {
+  
+  },
+  getUserInfo:function(cb){
     var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
       //调用登录接口
       wx.login({
         success: function () {
           wx.getUserInfo({
             success: function (res) {
               that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
             }
           })
         }
       })
-    
+    }
   },
-  // getUserInfo:function(cb){
-  //   var that = this
-  //   if(this.globalData.userInfo){
-  //     typeof cb == "function" && cb(this.globalData.userInfo)
-  //   }else{
-  //     //调用登录接口
-  //     wx.login({
-  //       success: function () {
-  //         wx.getUserInfo({
-  //           success: function (res) {
-  //             that.globalData.userInfo = res.userInfo
-  //             typeof cb == "function" && cb(that.globalData.userInfo)
-  //           }
-  //         })
-  //       }
-  //     })
-  //   }
-  // },
-  globalData:{
-    userInfo:null
-  }
 })
